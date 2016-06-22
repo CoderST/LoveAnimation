d00bfa7eff21e382f7ad64e7a61b7c3f5e2ca5fb
shengtao <694468528@qq.com>
2016-06-22 15:23:14 +0800

    添加长按事件

--- LoveAnimation/LoveAnimation/ViewController.swift
+++ LoveAnimation/LoveAnimation/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var time:NSTimer?
     let window_width = UIScreen.mainScreen().bounds.width
     let window_height = UIScreen.mainScreen().bounds.height
     let loveHW:CGFloat = 30
@@ -17,26 +18,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.lightGrayColor()
-       let tapGesture = UITapGestureRecognizer(target: self, action: Selector("handleTapGesture:"))
+        // 添加点击手势
+        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTapGesture:")
         view.addGestureRecognizer(tapGesture)
         
-        let longGesture = UILongPressGestureRecognizer(target: self, action: Selector("handleLongGesture:"))
+        let longGesture = UILongPressGestureRecognizer(target: self, action: "handleLongGesture:")
         view.addGestureRecognizer(longGesture)
     }
-
-
+    
+    
     
     // MARK: - 手势事件
-    func handleTapGesture(sender: UITapGestureRecognizer){
-       let loveView = LoveView(frame: CGRect(x: 2 * loveHW, y: window_height - 2 * loveHW, width: loveHW, height: loveHW))
-        // 先添加到界面上 不然不会调用draw方法
-         view.addSubview(loveView)
-        loveView.showAnimationWith(view)
-       
+    func handleTapGesture(tap: UITapGestureRecognizer){
+        
+        creatAnimation()
     }
     
-    func handleLongGesture(sender: UITapGestureRecognizer){
-        
+    func handleLongGesture(long: UITapGestureRecognizer){
+        if long.state == UIGestureRecognizerState.Began{
+            time = NSTimer.scheduledTimerWithTimeInterval(0.2, target: self, selector:  "creatAnimation", userInfo: nil, repeats: true)
+        }else{
+            // 先停止后置空
+            time?.invalidate()
+            time = nil
+            
+        }
+    }
+    
+    
+    func creatAnimation(){
+        let loveView = LoveView(frame: CGRect(x: 2 * loveHW, y: window_height - 2 * loveHW, width: loveHW, height: loveHW))
+        // 先添加到界面上 不然不会调用draw方法
+        view.addSubview(loveView)
+        loveView.showAnimationWith(view)
     }
 }
 
